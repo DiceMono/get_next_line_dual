0ceebdab4088a5c94388b9ab0f3da9fa11467a82
DiceMono <qrdfg@naver.com>
2020-02-29 04:58:36 +0900

    fix for shell

--- random_generator.sh
+++ random_generator.sh
@@ -2,13 +2,15 @@ chr() {
 	printf \\$(printf '%03o' $1)
 }
 TEST_DIR=random_tests
+SET1=$(seq 1 15)
+SET2=$(seq 1 1000)
 rm -rf $TEST_DIR
 mkdir $TEST_DIR
 printf "generating random tests"
-for i in {1..15}; do
+for i in $SET1; do
 	printf .
 	touch $TEST_DIR/$i.txt
-	for j in {1..1000}; do
+	for j in $SET2; do
 		RAN_CHAR=$((RANDOM % 95 + 32))
 		if [ $((RANDOM % 100)) -lt 5 ];then
 			chr 10 >> $TEST_DIR/$i.txt
